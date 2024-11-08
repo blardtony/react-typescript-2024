575f6d15377cfc534f2a8dbc2b53efc7d4f5215d
blardtony <tonyblard55@gmail.com>
2024-11-08 17:05:15 +0100

    :sparkles: Navbar / Use react router Link

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <header className="container navbar mx-auto px-6 lg:px-2">
-      <a className="flex-1">React Playground</a>
+      <Link to={'/'} className="flex-1 text-xl font-bold">
+        React Playground
+      </Link>
     </header>
   );
 };
