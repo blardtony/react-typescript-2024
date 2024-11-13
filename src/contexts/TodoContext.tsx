439e03ea5d7d70d30649382ad6163c28998712f0
blardtony <tonyblard55@gmail.com>
2024-11-13 10:44:09 +0100

    :recycle: Add id and date in Todo

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,4 +1,5 @@
 import { createContext } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import {
   Todo,
   TodoContextType,
@@ -10,17 +11,23 @@ const TodoContext = createContext<TodoContextType | undefined>(undefined);
 
 const TodoProvider = ({ children, todos, setTodos }: TodoProviderType) => {
   const addTodo = (data: TodoFormData) => {
-    setTodos([...todos, { name: data.name, completed: false }]);
+    const newTodo: Todo = {
+      id: uuidv4(),
+      createdAt: new Date(),
+      name: data.name,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
   };
 
   const removeTodo = (todo: Todo) => {
-    const newTodos = todos.filter((t) => t.name !== todo.name);
+    const newTodos = todos.filter((t) => t.id !== todo.id);
     setTodos(newTodos);
   };
 
   const toggleTodo = (todo: Todo) => {
     const newTodos = todos.map((t) =>
-      t.name === todo.name ? { ...t, completed: !t.completed } : t,
+      t.id === todo.id ? { ...t, completed: !t.completed } : t,
     );
     setTodos(newTodos);
   };
--- src/types/todo.type.ts
+++ src/types/todo.type.ts
@@ -1,6 +1,8 @@
 export type Todo = {
+  id: string;
   name: string;
   completed: boolean;
+  createdAt: Date;
 };
 
 export type Todos = Todo[];
